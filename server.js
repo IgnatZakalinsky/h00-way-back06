9a3d01ddbfc7ac8a2a217a7fafded75a3f033ae9
neko <ai73a@yandex.ru>
2024-04-02 15:55:49 +0300

    read html, error read

--- server.js
+++ server.js
@@ -1,18 +1,31 @@
 const http = require('http')
+const fs = require('fs')
 
 const server = http.createServer(async (req, res) => {
     switch (req.url) {
         case '/': { // http://localhost:3000/
             console.log(1)
+            await new Promise(resolve => {
+                fs.readFile('pages/home.html', (error, data) => {
+                    if (error) {
+                        console.log(error)
+                        res.write('some error in read html ')
+                    } else res.write(data)
+                    resolve(true) // or any
+                })
+            })
             break
         }
         case '/2': { // http://localhost:3000/2
             console.log(2)
-            await new Promise(resolve => { // иногда становиться в очередь, хз почему (02.04.2024)
-                setTimeout(() => {
-                    console.log('stop ' + new Date())
-                    resolve(true) // or any 
-                }, 5000)
+            await new Promise(resolve => {
+                fs.readFile('pages/home2.html', (error, data) => { // (02.04.2024)
+                    if (error) {
+                        console.log(error)
+                        res.write('some error in read html ')
+                    } else res.write(data)
+                    resolve(true) // or any
+                })
             })
             break
         }
